e5f5ba8534adec1bf72e66ce8d932b46c219fffc
seattleowl <32573897+seattleowl@users.noreply.github.com>
2021-02-08 07:44:34 -0800

    add lab class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
-import { getDirname } from "./src/util/dirname.js";
+import { getDirname } from "./util/dirname.js";
+import { router as labRouter } from "./lab-base.js";
 const __dirname = getDirname(import.meta);
 
 const app = express();
@@ -8,7 +9,9 @@ const app = express();
 app.use("/assets", express.static("assets"));
 
 app.get("/", (_req, res) => {
-	res.sendFile(path.join(__dirname, "page/index.html"));
+	res.sendFile(path.join(__dirname, "../page/index.html"));
 });
+
+app.use("/lab", labRouter);
 
 app.listen(process.env.PORT || 5000);
---
+++ src/lab-base.js
@@ -0,0 +1,30 @@
+import express from "express";
+
+export const router = express.Router();
+router.get("/", (req, res) => {
+	res.status(400).json({
+		error: "Please specify a lab id. (api.seattleowl.com/lab/xxx)"
+	});
+});
+
+export class Lab {
+	static instance;
+	app = null;
+
+	connect() {
+		throw new TypeError(
+			"Trying to regester a class without a 'connect' method."
+		);
+	}
+
+	static regester(subdomain) {
+		if (this.instance)
+			throw new Error("Trying to regester a already regestered class.");
+		this.instance = new this();
+		this.instance.app = express.Router();
+		this.instance.connect();
+		router.use(`/${subdomain}`, this.instance.app);
+	}
+}
+
+Lab.regester("test");
